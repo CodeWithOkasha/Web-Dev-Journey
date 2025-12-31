5246ceed93b978a7d8b8d823dc04970e9eea6c90
Muhammad Okasha <okashakhan461@gmail.com>
2025-12-31 16:42:21 +0500

    Add pure JavaScript calculator with operator precedence

---
+++ Projects/Calculator/professional_Cal.js
@@ -0,0 +1,60 @@
+let str = "12*3+43";
+let newNum = [];
+let newOp = [];
+let temp = "";
+
+//Step1: Extract numbers and operators
+for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+        temp += str[i];
+    }
+    else {
+        newNum.push(Number(temp));
+        temp = "";
+        newOp.push(str[i]);
+    }
+}
+if (temp != "") {
+    newNum.push(Number(temp));
+}
+
+
+//Solution for / and *
+for (let i = 0; i < newOp.length; i++) {
+    if (newOp[i] == "/") {
+        let result = newNum[i] / newNum[i + 1];
+        newNum[i] = result;
+        newNum.splice(i + 1, 1);
+        newOp.splice(i, 1);
+        i--;
+    }
+    else if (newOp[i] == "*" && newOp != "/") {
+        let result = newNum[i] * newNum[i + 1];
+        newNum[i] = result;
+        newNum.splice(i + 1, 1);
+        newOp.splice(i, 1);
+        i--;
+    }
+
+}
+
+//Solution for + and -
+for (let i = 0; i < newOp.length; i++) {
+    if (newOp[i] == "+") {
+        let result = newNum[i] + newNum[i + 1];
+        newNum[i] = result;
+        newNum.splice(i + 1, 1);
+        newOp.splice(i, 1);
+        i--;
+    }
+    else if (newOp[i] == "-" && newOp != "+") {
+        let result = newNum[i] - newNum[i + 1];
+        newNum[i] = result;
+        newNum.splice(i + 1, 1);
+        newOp.splice(i, 1);
+        i--;
+    }
+
+}
+console.log(newNum);
+
